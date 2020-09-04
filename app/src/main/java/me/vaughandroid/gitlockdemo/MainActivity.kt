30255095f2e5e72686e906208aa8715675f081c8
Chris Vaughan <Chris.Vaughan01@bbc.co.uk>
2020-09-04 11:57:29 +0100

    Add Kotlin library module.

--- app/src/main/java/me/vaughandroid/gitlockdemo/MainActivity.kt
+++ app/src/main/java/me/vaughandroid/gitlockdemo/MainActivity.kt
@@ -2,10 +2,18 @@ package me.vaughandroid.gitlockdemo
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+import me.vaughandroid.kotlin_library.KotlinLibrary
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        initViews()
+    }
+
+    private fun initViews() {
         setContentView(R.layout.activity_main)
+
+        kotlinLibraryVersion.text = KotlinLibrary.version
     }
 }
---
+++ kotlin-library/src/main/java/me/vaughandroid/kotlin_library/KotlinLibrary.kt
@@ -0,0 +1,7 @@
+package me.vaughandroid.kotlin_library
+
+object KotlinLibrary {
+
+    val version = "1"
+
+}
